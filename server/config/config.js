6d9cd7287a939c025446f5c4c7b330cdd2d936a1
Diego Alomoto Chicaiza <wladimiralomoto1992@outlook.es>
2020-08-28 21:49:28 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://diegoalomoto2308:Ago23Mil992@cluster0.lycjh.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
